e61962ec08f134ec5fb9819652e2e3844bcf52f3
Rafal Borowski <rafall.borowski@gmail.com>
2022-03-07 20:51:45 +0100

    add app routing and form component

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  return (
+    <form
+      style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <input {...register('login', { required: true })} placeholder="login" type="text" />
+      {errors.login && <span>Login is required</span>}
+      <input {...register('password', { required: true })} placeholder="password" type="password" />
+      {errors.password && <span>Password is required</span>}
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default Login;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from 'components/templates/Layout';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import MissingPage from './404';
 
-function Root() {
-  return <div className="Root">???</div>;
-}
+const Root = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="sign-in" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<MissingPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default Root;
